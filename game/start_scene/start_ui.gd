164ca2ed1f61dc1ac41f4ecff56622d342b66ed5
Eli <88557639+lishaduck@users.noreply.github.com>
2026-04-12 03:00:14 -0500

    fix start crash

--- game/start_scene/start_ui.gd
+++ game/start_scene/start_ui.gd
@@ -1,15 +1,7 @@
 extends CenterContainer
 
-@onready var save_list_node = $LoadGame/SaveList
-var save_list : Array
-
 func _ready():
 	_set_pane(1)
-	if PersistentWorld.instance:
-		save_list = PersistentWorld.instance.list_saves()
-		save_list_node.clear()
-		for entry in save_list:
-			save_list_node.add_item(entry)
 
 func _set_pane(p_no):
 	$MainMenu.visible = p_no == 1

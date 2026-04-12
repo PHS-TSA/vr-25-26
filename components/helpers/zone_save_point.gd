e6ee231296300f3c4a84b81bbf61bdd19f5103a1
Sam G. <104700099+hey-1m-sam@users.noreply.github.com>
2026-04-11 22:40:52 -0500

    Change Menu

--- components/helpers/zone_save_point.gd
+++ components/helpers/zone_save_point.gd
@@ -6,14 +6,4 @@ extends Node3D
 var is_saved = false
 
 func _on_save_button_button_pressed(_button):
-	# Skip if already saved
-	if is_saved:
-		return
-
-	# Auto-save the game
-	if not GameState.auto_save_game():
-		return
-
-	# Change button to saved
-	$Pole/SaveButton/Button.set_surface_override_material(0, saved_material)
-	is_saved = true
+	pass
